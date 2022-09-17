a2f1e8598a8622ccd9b2b8aaf50ef21f5aaadab3
Matúš Nekoranec <113455013+brejnded@users.noreply.github.com>
2022-09-17 21:33:40 +0200

    Update Exercise.cs

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_01._01
 {
@@ -6,6 +6,14 @@ namespace Open_Lab_01._01
     {
         public bool IsLessThan10(string input)
         {
+            int num = int.Parse(input);
+
+            if (num < 10) {
+                return true;
+            }
+            else {
+                return false;
+            }
             throw new NotImplementedException();
         }
     }
